1f2bfadced050632d354ed84d264650a08aac8b8
Yuji Kosakowski <yuji.k.dev@gmail.com>
2024-10-16 17:55:32 -0600

    Add more methods:

--- Metaprogramming/playground.rb
+++ Metaprogramming/playground.rb
@@ -1,3 +1,18 @@
 array = [1, 2, 3, 2]
 
-puts array.any? { _1 > 2}
+
+
+# array.each do |num|
+#   if num >= 2
+#     peek
+#     require 'pry'; binding.pry
+#   end
+# end
+
+# hash = {"odd": 4, "even": 4}
+
+# p hash
+
+a = ["kitty cat", "dog", "horse"]
+p a.minmax                                  #=> ["albatross", "horse"]
+p a.minmax { |a, b| a.length <=> b.length } #=> ["dog", "albatross"]
